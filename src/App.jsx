002ca09ce2ee98f209627c1312fd82f80ab9fc85
winfredo <winfrednukpe2002@gmail.com>
2025-08-12 20:26:33 +0000

    Fixes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import "./App.css";
 import Data from "./data";
 
 const App = () => {
-  const [currentWord, setCurrentWord] = React.useState("react");
+  const [currentWord, setCurrentWord] = React.useState("fred");
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const [guessLetters, setGuessLetters] = React.useState([]);
+  const wrongGuessCount = guessLetters.filter(
+    (letter) => !currentWord.includes(letter)
+  ).length;
+  const isGameLost = wrongGuessCount >= Data.length - 1;
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessLetters.includes(letter));
+  const isGameOver = isGameWon || isGameLost;
+
+  console.log(wrongGuessCount);
 
   function handleLetterClick(letter) {
     setGuessLetters((prevLetters) =>
@@ -21,51 +31,73 @@ const App = () => {
           from Assembly!
         </p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
+
+      {/* Game status section */}
+      <div className="game-status">
+        <section className={isGameWon ? "win-status" : "lose-status"}>
+          {isGameOver ? (
+            <>
+              <h2>{isGameWon ? "You win!" : "Game Over!"}</h2>
+              <p>
+                {isGameWon
+                  ? "Well done! 🎉"
+                  : "You lose! Better start learning Assembly 😭"}
+              </p>
+            </>
+          ) : null}
+        </section>
+      </div>
+
+      {/* Language chips section */}
       <section className="language-chips">
-        {Data.map((lang) => (
-          <span
-            className="chip"
-            key={lang.name}
-            style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
-          >
-            {lang.name}
-          </span>
-        ))}
+        {Data.map((lang, index) => {
+          const guessChecker = index < wrongGuessCount;
+          console.log(guessChecker);
+          return (
+            <span
+              className={`chip ${guessChecker ? "lost" : ""}`}
+              key={lang.name}
+              style={{
+                backgroundColor: lang.backgroundColor,
+                color: lang.color,
+              }}
+            >
+              {lang.name}
+            </span>
+          );
+        })}
       </section>
+
+      {/* Guess word section */}
       <section className="guess-el">
         {currentWord.split("").map((letter, index) => (
           <span key={index} className="word-guess">
-            {letter.toUpperCase()}
+            {guessLetters.includes(letter) ? letter.toUpperCase() : ""}
           </span>
         ))}
       </section>
+
       {/* Alphabet Buttons */}
-      <section onClick={handleLetterClick} className="alphabet-buttons">
-    {alphabet.split("").map(letter => {
-    const isGuessed = guessLetters.includes(letter);
-    const isCorrect = isGuessed && currentWord.includes(letter);
-    const isWrong = isGuessed && !currentWord.includes(letter);
-    
-    const className = isCorrect ? 'char-btn correct' : (isWrong ? 'char-btn wrong' : 'char-btn')
-    console.log(className);
-    
-    return (
-     
-        <button
-            className={className}
-            key={letter}
-            onClick={() => handleLetterClick(letter)}
-        >
-            {letter.toUpperCase()}
-        </button>
-    )
-})};
+      <section className="alphabet-buttons">
+        {alphabet.split("").map((letter) => {
+          const isGuessed = guessLetters.includes(letter);
+          const isCorrect = isGuessed && currentWord.includes(letter);
+          const isWrong = isGuessed && !currentWord.includes(letter);
+
+          return (
+            <button
+              className={`char-btn ${
+                isCorrect ? "correct" : isWrong ? "wrong" : "char-btn"
+              }`}
+              key={letter}
+              onClick={() => handleLetterClick(letter)}
+            >
+              {letter.toUpperCase()}
+            </button>
+          );
+        })}
       </section>
-      <button className="new-game-btn">New Game</button>
+      {isGameOver ? <button className="new-game-btn">New Game</button> : null}
     </main>
   );
 };
